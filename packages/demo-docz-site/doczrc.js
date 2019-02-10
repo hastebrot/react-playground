5c91c8e69e84fd8f3512802fbcbd62ba8c3b926f
Benjamin Gudehus <hastebrot@gmail.com>
2019-02-10 18:51:55 +0100

    (feature) Add demo-docz-site package.

---
+++ packages/demo-docz-site/doczrc.js
@@ -0,0 +1,12 @@
+export default {
+  title: "demo docz site",
+  description: "some documentation",
+  themeConfig: {
+    mode: "light",
+    colors: {
+      primary: "tomato",
+    },
+    showPlaygroundEditor: true,
+  },
+  hashRouter: true
+}
