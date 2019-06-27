a105eb18144960d5e88724161eef191e9e11eb63
Benjamin Gudehus <hastebrot@gmail.com>
2019-06-28 01:16:10 +0200

    (feature) Add demo-storybook-site-new.o

---
+++ packages/demo-storybook-site-new/.storybook/config.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { load, addDecorator, addParameters } from '@storybook/react';
+import { DocsPage } from '@storybook/addon-docs/blocks';
+import { withA11y } from '@storybook/addon-a11y';
+
+addParameters({
+  docs: DocsPage,
+});
+
+addDecorator(withA11y);
+
+import { GlobalStyle } from '../src/components/shared/global';
+addDecorator(story => (
+  <>
+    <GlobalStyle />
+    {story()}
+  </>
+));
+
+// automatically import all files ending in *.stories.js
+load(require.context('../src', true, /\.stories\.js$/), module);
+load(require.context('../src', true, /\.stories\.mdx$/), module);
